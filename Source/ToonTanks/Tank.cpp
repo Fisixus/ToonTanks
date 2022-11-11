7ff70ca583faa1def00dd483d2aab2140db3d7fe
Utku ERDEN <31778258+Fisixus@users.noreply.github.com>
2022-11-11 01:54:25 -0500

    Acceleration has been added(plus)

--- Source/ToonTanks/Tank.cpp
+++ Source/ToonTanks/Tank.cpp
@@ -5,6 +5,8 @@
 
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Math/UnrealMathUtility.h"
 
 ATank::ATank()
 {
@@ -15,4 +17,31 @@ ATank::ATank()
 	m_CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	m_CameraComponent->SetupAttachment(m_SpringArmComponent);
 
+}
+
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ATank::Move);
+}
+
+void ATank::Move(float Direction)
+{
+	auto DeltaTime = UGameplayStatics::GetWorldDeltaSeconds(this);
+	
+	// If no input has been given
+	if(FMath::IsWithin(Direction,-0.9f,0.9f))
+	{
+		m_Speed = FMath::Lerp(m_Speed, 0, DeltaTime*4.f);
+	}
+	else
+	{
+		m_Speed = FMath::Lerp(m_Speed, m_Speed + m_Acceleration*Direction, DeltaTime*4.f);
+	}
+	
+	m_Speed = FMath::Clamp(m_Speed,-m_MaxSpeed,m_MaxSpeed);
+
+	FVector NewOffset(0.f);
+	NewOffset.X = m_Speed;
+	AddActorLocalOffset(NewOffset);
 }
--- Source/ToonTanks/Tank.h
+++ Source/ToonTanks/Tank.h
@@ -15,6 +15,8 @@ class TOONTANKS_API ATank : public ABasePawn
 	GENERATED_BODY()
 public:
 	ATank();
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
@@ -22,5 +24,14 @@ private:
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
 	class UCameraComponent* m_CameraComponent;
-	
+
+
+	UPROPERTY(VisibleAnywhere, Category = "Movement")
+	float m_Speed = 0.f;
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float m_MaxSpeed = 450.5f;
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float m_Acceleration = 40.f;
+
+	void Move(float Value);
 };
